bc06cbe8fb1c34ef1d8fb850236ee240c297f3fe
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2023-10-01 18:13:59 -0600

    Changed Point Test from 0 to 1

--- Lab 2/exB/GraphicsWorld.cpp
+++ Lab 2/exB/GraphicsWorld.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void GraphicsWorld::run() {
-#if 0 // Change 0 to 1 to test Point
+#if 1 // Change 0 to 1 to test Point
     Point m(6, 8);
     Point n(6, 8);
     n.setX(9);
